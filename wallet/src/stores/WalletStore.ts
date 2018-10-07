8e616ced2ff79101227aba92ee3815cd79dd66cd
William Hua <william@attente.ca>
2018-10-06 23:55:48 -0700

    wallet: actually implement JSON-RPC for transactions

--- wallet/src/stores/WalletStore.ts
+++ wallet/src/stores/WalletStore.ts
@@ -29,6 +29,7 @@ class WalletStore {
       await this.decryptWallet(tempPassword)
       return this.account
     }
+
     const privateKey = '0xb0057716d5917badaf911b193b12b910811c1497b5bada8d7711f758981c3773'
     const provider = new ethers.providers.JsonRpcProvider()
     this.decryptedWallet = new ethers.Wallet(privateKey, provider)
@@ -40,11 +41,12 @@ class WalletStore {
   }
 
   private async encryptWallet(password) {
-    if (this.account && this.decryptedWallet) {
-      this.encryptedWallet = this.decryptedWallet.mnemonic
+    // XXX: disable this because you can't derive a mnemonic from a private key
+    if (this.account && this.decryptedWallet && false) {
+      this.encryptedWallet = this.decryptedWallet!.mnemonic
 
-      window.localStorage.setItem('account', this.account)
-      window.localStorage.setItem('encryptedWallet', this.encryptedWallet)
+      window.localStorage.setItem('account', this.account!)
+      window.localStorage.setItem('encryptedWallet', this.encryptedWallet!)
     }
   }
 
@@ -92,36 +94,17 @@ class WalletStore {
         break
 
       case 'eth_estimateGas':
-        response.result = '0x5208'
+        response.result = (await this.decryptedWallet!.provider.estimateGas(params[0])).toHexString()
         break
 
       case 'eth_sendTransaction':
-        response.result = ethers.utils.keccak256(ethers.utils.toUtf8Bytes('dummy hash'))
+        response.result = (await this.decryptedWallet!.sendTransaction(params[0])).hash
         break
 
       case 'eth_getTransactionByHash':
-        response.result = {
-          'blockHash': '0x1d59ff54b1eb26b013ce3cb5fc9dab3705b415a67127a003c3e61eb445bb8df2',
-          'blockNumber': '0x5daf3b', // 6139707
-          'from': '0xa7d9ddbe1f17865597fbd27ec712455208b6b76d',
-          'gas': '0xc350', // 50000
-          'gasPrice': '0x4a817c800', // 20000000000
-          'hash': ethers.utils.keccak256(ethers.utils.toUtf8Bytes('dummy hash')),
-          'input': '0x68656c6c6f21',
-          'nonce': '0x15', // 21
-          'to': '0xf02c1c8e6114b1dbe8937a39260b5b0a374432bb',
-          'transactionIndex': '0x41', // 65
-          'value': '0xf3dbb76162000', // 4290000000000000
-          'v': '0x25', // 37
-          'r': '0x1b5e176d927f8e9ab405058b2d2457392da3e20f328b16ddabcebc33eaac5fea',
-          's': '0x4ba69724e8f69de52f0125ad8b3c5c2cef33019bac3249e2c0a2192766d1721c'
-        }
+        response.result = await this.decryptedWallet!.provider.getTransaction(params[0])
         break
 
-      case 'eth_blockNumber':
-        response.result = '0x1234'
-        break
-  
       default:
         console.log(request)
         break
