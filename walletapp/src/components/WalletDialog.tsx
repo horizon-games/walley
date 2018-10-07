848c52b0cb5b65e0e88ef04d431c04ad1675ca28
William Hua <william@attente.ca>
2018-10-06 22:30:40 -0700

    walletapp: SignDialog -> WalletDialog

--- walletapp/src/components/WalletDialog.tsx
+++ walletapp/src/components/WalletDialog.tsx
@@ -3,13 +3,13 @@ import styled from 'styled-components'
 import { observer, inject } from 'mobx-react'
 import WalletStore from '~/stores/WalletStore'
 
-export interface ISignDialogProps {
+export interface IWalletDialogProps {
   walletStore?: WalletStore
 }
 
 @inject('walletStore')
 @observer
-export default class SignDialog extends React.Component<ISignDialogProps, {}> {
+export default class WalletDialog extends React.Component<IWalletDialogProps, {}> {
   render() {
     return (
       <div>word.</div>
