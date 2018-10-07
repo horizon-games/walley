9be7e968502d44be2af62b37d4a93eba97e23165
Peter Kieltyka <peter.kieltyka@gmail.com>
2018-10-06 20:15:39 -0700

    example: add send ETH tx

--- example-dapp/src/routes/IndexRoute.tsx
+++ example-dapp/src/routes/IndexRoute.tsx
@@ -3,6 +3,7 @@ import { observer, inject } from 'mobx-react'
 import { withRouter } from 'mobx-little-router-react'
 import styled from 'styled-components'
 import WalletStore from '~/stores/WalletStore'
+import * as ethers from 'ethers'
 // import layout from '~/utils/layout'
 
 export interface IIndexRouteProps {
@@ -14,6 +15,9 @@ export interface IIndexRouteProps {
 @inject('walletStore')
 @observer
 class IndexRoute extends React.Component<IIndexRouteProps, {}> {
+  ethSendAmountRef: HTMLInputElement | null
+  ethSendAddressRef: HTMLInputElement | null
+
   getAddress = async () => {
     const { walletStore } = this.props
     console.log('getAddress:')
@@ -28,12 +32,42 @@ class IndexRoute extends React.Component<IIndexRouteProps, {}> {
     console.log('=>', sig)
   }
 
+  sendETH = async () => {
+    const { walletStore } = this.props
+    console.log('sendEth:')
+
+    const amount = this.ethSendAmountRef!.value
+    const sendTo = this.ethSendAddressRef!.value
+
+    if (amount === '') {
+      console.log('Amount invalid.')
+      return
+    }
+    if (sendTo === '' || !sendTo.startsWith('0x')) {
+      console.log('SendTo Address invalid.')
+      return
+    }
+
+    const rtx = {
+      to: sendTo,
+      value: ethers.utils.parseEther(amount)
+    }
+
+    const resp = await walletStore.signer.sendTransaction(rtx)
+  }
+
   render() {
     return (
       <Container>
         DAPP example... mmmmhmmm<br /><br />
         <button type="button" onClick={this.getAddress}>Get Address</button><br />
         <button type="button" onClick={this.signMessage}>Sign Message</button><br />
+        <div>
+          <button type="button" onClick={this.sendETH}>Send ETH</button>
+          <input ref={ref => this.ethSendAmountRef = ref} type="text" value={'0.1'} placeholder="ETH amount" /> to
+          <input ref={ref => this.ethSendAddressRef = ref} type="text" value={'0x4b25350d7c0224a68ea0c72855bfd035d8fa09d3'} placeholder="Address" />
+          <br />
+        </div>
       </Container>
     )
   }
